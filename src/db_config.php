c68949ce26c01b130a76bf04b6ee96da9ebd6105
MasterZer0x <42559276+MasterZer0x@users.noreply.github.com>
2021-06-09 23:54:55 -0400

    Modificado keys de bd e implementacion de bootstrap

--- src/db_config.php
+++ src/db_config.php
@@ -1,6 +1,6 @@
 <?php
 /* Detalles de la conexión */
-$conn_string = "host=localhost port=5432 dbname=<nombre_db> user=postgres password=<contraseña>";
+$conn_string = "host=localhost port=5432 dbname=Tarea2 user=postgres password=2453";
 // Recuerde reemplazar "<contraseña>" por su contraseña y "<nombre_db>" por el nombre de su BD. No se incluyen los "<>".
 // Establecemos una conexión con el servidor postgresSQL
 $dbconn = pg_connect($conn_string);
