cc49f613843d9be561833b9d2eba9f7fb8c970b4
MasterZer0x <42559276+MasterZer0x@users.noreply.github.com>
2021-06-13 19:23:58 -0400

    Wallet

--- src/sesion/valida_login.php
+++ src/sesion/valida_login.php
@@ -21,7 +21,7 @@ session_start();
             if (password_verify($password, $row["contraseña"])){
                 $_SESSION["email"] = $email;
                 
-                $sql = "SELECT nombre,apellido,pais,fecha_registro,rango FROM usuario WHERE correo='".$email."'";
+                $sql = "SELECT id,nombre,apellido,pais,fecha_registro,rango FROM usuario WHERE correo='".$email."'";
                 $result = pg_query_params($dbconn, $sql, array());
                 $row = pg_fetch_array($result);
 
@@ -29,7 +29,7 @@ session_start();
                 $result = pg_query_params($dbconn, $sql, array());
                 $nombre_pais = pg_fetch_result($result, 0);
 
-
+                $_SESSION["id"] = $row["id"];
                 $_SESSION["nombre"] = $row["nombre"];
                 $_SESSION["apellido"] = $row["apellido"];
                 $_SESSION["pais"] = $nombre_pais;
