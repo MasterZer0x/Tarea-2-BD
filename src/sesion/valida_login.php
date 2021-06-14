f9336a51b06b55c082814a12d352e52a12b6f32a
MasterZer0x <42559276+MasterZer0x@users.noreply.github.com>
2021-06-14 00:53:07 -0400

    Revert "Merge branch 'main' of https://github.com/MasterZer0x/Tarea-2-BD into main"
    This reverts commit 6dc22c8222ba3fb549a311d313b92c1f99780b2a, reversing
    changes made to 699b80fe2a02e43e7e1921c3ac302d051c79a148.

--- src/sesion/valida_login.php
+++ src/sesion/valida_login.php
@@ -1,7 +1,5 @@
-<?php
-session_start();
-?>
-<?php include '..\db_config.php';?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';?>
+<?php include 'valida_sesion.php' ?>
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $email = $_POST["email"];
@@ -13,13 +11,15 @@ session_start();
         // Preparamos y hacemos la query a la base de datos.
         $sql = 'SELECT usuario.correo, usuario.contraseña FROM usuario WHERE usuario.correo = $1';
         $result = pg_query_params($dbconn, $sql, array($email));
-
+        
         if( pg_num_rows($result) > 0 ) {
             $row = pg_fetch_assoc($result);
             if (password_verify($password, $row["contraseña"])){
+                
+              
                 $_SESSION["email"] = $email;
-
-                $sql = "SELECT id,nombre,apellido,pais,fecha_registro,rango FROM usuario WHERE correo='".$email."'";
+                
+                $sql = "SELECT nombre,apellido,pais,fecha_registro,rango FROM usuario WHERE correo='".$email."'";
                 $result = pg_query_params($dbconn, $sql, array());
                 $row = pg_fetch_array($result);
 
@@ -27,21 +27,22 @@ session_start();
                 $result = pg_query_params($dbconn, $sql, array());
                 $nombre_pais = pg_fetch_result($result, 0);
 
-                $_SESSION["id"] = $row["id"];
+
                 $_SESSION["nombre"] = $row["nombre"];
                 $_SESSION["apellido"] = $row["apellido"];
                 $_SESSION["pais"] = $nombre_pais;
                 $_SESSION["fecha_registro"] = $row["fecha_registro"];
                 $_SESSION["rango"] = $row["rango"];
 
-
+                
                 echo "<script type='text/javascript'>document.location='/user/profile.html';</script>";
 
             } else {
+                
                 echo "<script type='text/javascript'>document.location='/sesion/log-in.html';</script>";
             }
         } else {
             echo "<script type='text/javascript'>document.location='/sesion/log-in.html';</script>";
         }}
-
+    
 ?>
